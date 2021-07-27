37eef6ce714233b8b8fe55ae8bf8b180800e9fac
Hannah <hannahwoodward@hotmail.com>
2021-07-27 10:34:01 +0800

    with lines to check

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat (name) {
+function destructivelyAppendCat(name) {
   cats.push(name);
 }
+//destructivelyAppendCat("Ralph");
+//console.log(cats);
 
-function destructivelyPrependCat (name) {
+function destructivelyPrependCat(name) {
   cats.unshift(name);
 }
+//destructivelyPrependCat("Bob");
+//console.log(cats);
 
-function destructivelyRemoveLastCat () {
-  cats.pop();
+function destructivelyRemoveLastCat() {
+  cats.pop(name);
 }
+//destructivelyRemoveLastCat();
+//console.log(cats);
 
-function destructivelyRemoveFirstCat () {
-  cats.shift();
+function destructivelyRemoveFirstCat(){
+  cats.shift(name);
 }
+//destructivelyRemoveFirstCat();
+//console.log(cats);
 
-function appendCat (name) {
+function appendCat(name) {
   return [...cats, name];
 }
+//console.log(appendCat ('Broom'));
+//console.log(cats);
 
-function prependCat (name) {
+function prependCat(name) {
   return [name, ...cats];
 }
+//console.log(prependCat ('Arnold'));
+//console.log(cats);
 
 function removeFirstCat () {
   return cats.slice(1);
 }
+//console.log(removeFirstCat ());
 
 function removeLastCat () {
-  return cats.slice(0, cats.length - 1);
+  return cats.slice(0,-1);
 }
+//console.log(removeLastCat ());
